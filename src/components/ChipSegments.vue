7b42e4c74c7c4f111acb87bb003e4a156712e7f7
Todd H <github@simplybest.com>
2023-02-18 21:58:10 -0700

    add chip segments, cleanup home

---
+++ src/components/ChipSegments.vue
@@ -0,0 +1,68 @@
+<script lang="ts" setup>
+    import { IonSegment, IonSegmentButton, IonCard, IonCardHeader } from '@ionic/vue';
+    import { ref } from 'vue';
+
+    const segment = ref('1');
+</script>
+
+<template>
+    <ion-segment v-model="segment" mode="ios">
+        <ion-segment-button value="1">
+            Segment 1
+        </ion-segment-button>
+        <ion-segment-button value="2">
+            Segment 2
+        </ion-segment-button>
+        <ion-segment-button value="3">
+            Segment 3
+        </ion-segment-button>
+    </ion-segment>
+    <ion-card v-show="segment === '1'" class="ion-no-margin ion-padding-bottom">
+        <ion-card-header>
+            1
+        </ion-card-header>
+    </ion-card>
+    <ion-card v-show="segment === '2'" class="ion-no-margin ion-padding-bottom">
+        <ion-card-header>
+            2
+        </ion-card-header>
+    </ion-card>
+    <ion-card v-show="segment === '3'" class="ion-no-margin ion-padding-bottom">
+        <ion-card-header>
+            3
+        </ion-card-header>
+    </ion-card>
+</template>
+
+<style lang="scss" scoped>
+ion-segment {
+    --background: #ffffff !important;
+    background: #ffffff !important;
+    border-radius: 0px !important;
+    border-top-left-radius: 8px !important;
+    border-top-right-radius: 8px !important;
+    padding: 32px 16px 0px;
+
+    ion-segment-button {
+        text-transform: capitalize;
+        letter-spacing: 0.8px;
+        --background-checked: var(--ion-color-primary);
+        --color-hover: var(--ion-color-primary);
+        --color-checked: #fff;
+        --color: #838383;
+        --border-radius: 50px;
+        --indicator-height: 0px;
+        --indicator-color: transparent !important;
+        --indicator-box-shadow: none !important;
+    }
+}
+
+ion-card {
+    border-radius: 0px !important;
+    border-bottom-left-radius: 8px !important;
+    border-bottom-right-radius: 8px !important;
+    --background: #ffffff !important;
+    background: #ffffff !important;
+    box-shadow: none;
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,55 +1,17 @@
 <template>
   <ion-page>
-    <!-- <ion-header :translucent="true">
-      <ion-toolbar color="primary">
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header> -->
-    
-    <ion-content :fullscreen="true">
-      <!-- <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header> -->
-    
-      <SbSegments></SbSegments>
+    <ion-content class="ion-padding" color="light">
+
+      <PaymentSegments></PaymentSegments>
+      <br>
+      <ChipSegments></ChipSegments>
 
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts" setup>
-  import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-  import SbSegments from '../components/SbSegments.vue';
+  import { IonContent, IonPage } from '@ionic/vue';
+  import PaymentSegments from '../components/PaymentSegments.vue';
+  import ChipSegments from '../components/ChipSegments.vue';
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
