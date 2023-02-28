fec858d78917cc709f15a37894eec1cc2ddd0279
Todd H <github@simplybest.com>
2023-02-27 23:23:00 -0700

    fix ts issue

--- src/components/tabs/useTabs.ts
+++ src/components/tabs/useTabs.ts
@@ -1,14 +1,15 @@
-import { ref, onMounted } from 'vue'; // delete this
+// The code in this file is not necessary if you are using the tabs in a proper Ionic page structure.
+// This was only added to put multiple tabs on a single page for this interactive demo.
+import { ref, onMounted } from 'vue';
 
 export function useTabs() {
-    const tabBarRef = ref<any | null>(null); // delete this (and the related ref on the ion-tab-bar in the template)
-    // delete this onMounted method--it is only necessary when the tabs are used out of proper Ionic page structure
+    const tabBarRef = ref<any | null>(null);
     onMounted(() => {
         const tabBar = tabBarRef.value?.$el;
         const firstTab = tabBar.querySelector('ion-tab-button');
         firstTab.setAttribute('aria-selected', 'true');
         firstTab.classList.add('tab-selected');
-        tabBar.querySelectorAll('ion-tab-button').forEach(tab => {
+        tabBar.querySelectorAll('ion-tab-button').forEach((tab:HTMLElement) => {
             tab.classList.add('tab-has-label','tab-has-icon','tab-layout-icon-top','ion-activatable','ion-selectable','ion-focusable');
             tab.onclick = (ev: MouseEvent) => {
                 const prevSelected = tabBar.querySelector('.tab-selected');
