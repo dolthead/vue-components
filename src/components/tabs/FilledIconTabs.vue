b4df645eed8dd101c5b61ff2b74337350158f31a
Todd H <github@simplybest.com>
2023-02-28 19:16:27 -0700

    add filled icon tabs

---
+++ src/components/tabs/FilledIconTabs.vue
@@ -0,0 +1,54 @@
+<script lang="ts" setup>
+import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonRouterOutlet } from '@ionic/vue';
+import { homeOutline, home, cartOutline, cart, notificationsOutline, notifications, personOutline, person } from 'ionicons/icons';
+import { ref } from 'vue';
+
+import { useTabs } from './useTabs'; // this is only needed when the tabs are used out of proper Ionic page structure
+const { tabBarRef } = useTabs(); // this is only needed when the tabs are used out of proper Ionic page structure
+
+const tab = ref(1);
+</script>
+
+<template>
+    <ion-tabs class="tabs-inline">
+        <ion-router-outlet></ion-router-outlet>
+        <ion-tab-bar slot="bottom" ref="tabBarRef">
+            <ion-tab-button tab="tab1" @click="tab = 1">
+                <ion-icon :icon="tab === 1 ? home : homeOutline"></ion-icon>
+                <ion-label>Home</ion-label>
+            </ion-tab-button>
+            <ion-tab-button tab="tab2" @click="tab = 2">
+                <ion-icon :icon="tab === 2 ? notifications : notificationsOutline"></ion-icon>
+                <ion-label>Notifications</ion-label>
+            </ion-tab-button>
+            <ion-tab-button tab="tab3" @click="tab = 3">
+                <ion-icon :icon="tab === 3 ? cart : cartOutline"></ion-icon>
+                <ion-label>Cart</ion-label>
+            </ion-tab-button>
+            <ion-tab-button tab="tab4" @click="tab = 4">
+                <ion-icon :icon="tab === 4 ? person : personOutline"></ion-icon>
+                <ion-label>Profile</ion-label>
+            </ion-tab-button>
+        </ion-tab-bar>
+    </ion-tabs>
+</template>
+
+<style lang="scss" scoped>
+.tabs-inline {
+    position: relative !important;
+    display: inline !important;
+}
+
+ion-tab-bar {
+    --border: 0px !important;
+    height: 70px;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+
+    ion-tab-button {
+        -webkit-transition: all 0.15s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+        transition: all 0.15s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+        --background: #fff !important;
+        --color: #a7a7a7 !important;
+    }
+}
+</style>
--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
-  import { IonContent, IonPage, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, IonText } from '@ionic/vue';
+  import { IonContent, IonPage, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, IonText, IonIcon } from '@ionic/vue';
   import BackgroundTabs from '@/components/tabs/BackgroundTabs.vue';
   import IconTabs from '@/components/tabs/IconTabs.vue';
   import DefaultTabs from '@/components/tabs/DefaultTabs.vue';
+  import FilledIconTabs from '@/components/tabs/FilledIconTabs.vue';
   import TopLineTabs from '@/components/tabs/TopLineTabs.vue';
   import ChipTabs from '@/components/tabs/ChipTabs.vue';
+  import { star } from 'ionicons/icons';
 </script>
 
 <template>
@@ -28,6 +30,15 @@
       <ion-text class="text-padding">Default Tabs</ion-text>
       <DefaultTabs></DefaultTabs>
       <br>
+      <ion-text class="text-padding ion-justify-content-center">
+        Filled Icon Tabs
+        <span class="new-feature ion-justify-content-center">
+          <ion-icon :icon="star" color="light"></ion-icon>
+          new!
+        </span>
+      </ion-text>
+      <FilledIconTabs></FilledIconTabs>
+      <br>
       <ion-text class="text-padding">Top Line Tabs</ion-text>
       <TopLineTabs></TopLineTabs>
       <br>
@@ -45,5 +56,15 @@
     &:first-child {
       margin-top: 16px;
     }
+  }
+  .new-feature {
+    background: #ff3b30;
+    color: #fff;
+    padding: 2px 6px;
+    border-radius: 4px;
+    font-size: 1em;
+    margin-left: 4px;
+    font-style: italic;
+    text-transform: uppercase;
   }
 </style>
