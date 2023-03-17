2a97e95458a985e4b7fd0c75b385eaae8ba6c175
Todd H <github@simplybest.com>
2023-03-16 19:10:08 -0600

    fix clipboard copy for safari :oP

--- src/components/CopyButton.vue
+++ src/components/CopyButton.vue
@@ -1,18 +1,57 @@
 <script lang="ts" setup>
-import { IonButton, IonIcon } from '@ionic/vue';
+import { IonButton, IonIcon, toastController } from '@ionic/vue';
 import { copyOutline } from 'ionicons/icons';
-import copySource from '@/services/sourceCodeService';
+
+const copySource = async (path: string) => {
+    const fullPath = `https://raw.githubusercontent.com/dolthead/vue-components/main/src/components/${path}.vue`;
+    navigator.clipboard
+        .write([new ClipboardItem({
+                "text/plain": new Promise(async (resolve, reject) => {
+                    try {
+                        const response = await fetch(fullPath);
+                        const source: BlobPart = `// ${path}.vue\n// ${fullPath}\n// from https://ionic-vue-components.web.app/\n\n` 
+                            + await response.text();
+                        resolve(new Blob([source], { type: "text/plain" }));
+                    } catch (err) {
+                        reject(err);
+                    }
+                }),
+            })
+        ])
+        .then(showCopiedToast, error => showErrorToast(error))
+        .catch(error => showErrorToast(error));
+}
+
+async function showCopiedToast() {
+    const toast = await toastController.create({
+        message: `Copied component code to the clipboard`,
+        duration: 2500,
+        position: 'bottom'
+    });
+    toast.present();
+}
+
+async function showErrorToast(error: string) {
+    const toast = await toastController.create({
+        message: `Error: ${error}`,
+        duration: 5000,
+        position: 'bottom',
+        color: 'danger'
+    });
+    toast.present();
+}
 </script>
 
 <template>
-    <ion-button fill="clear" size="small" class="ion-float-right" @click.stop="copySource($attrs.path as string)"
-            title="Copy the component's source to the clipboard">
+    <ion-button fill="clear" size="small" class="ion-float-right" 
+        @click.stop="copySource($attrs.path as string)"
+        title="Copy the component's source to the clipboard">
         <ion-icon slot="icon-only" :icon="copyOutline"></ion-icon>
     </ion-button>
 </template>
 
 <style lang="scss" scoped>
-    ion-button {
-        margin: auto auto;
-    }
+ion-button {
+    margin: auto auto;
+}
 </style>
